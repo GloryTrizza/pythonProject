7c6bb36cfe7666c962071dc415a0e1a1b8d89207
@GloryTrizza <gtrizza.onyango@gmail.com>
2023-03-27 18:06:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+def romanToInt(s: str) -> int:
+    # dictionary to hold the values of Roman numerals
+    # e.g. 'I' is the key, 1 is the value.
+    roman_dict = {'I': 1,
+                  'V': 5,
+                  'X': 10,
+                  'L': 50,
+                  'C': 100,
+                  'D': 500,
+                  'M': 1000}
+
+    # this is a variable to hold the result
+    result = 0
+
+    # this loop is iterating through the string from left to right
+    for i in range(len(s)):
+        # if the current Roman numeral is less than the next one, subtract its value
+        if i < len(s) - 1 and roman_dict[s[i]] < roman_dict[s[i+1]]:
+            result -= roman_dict[s[i]]
+        # if the current Roman numeral is less than the next one, add its value
+        else:
+            result += roman_dict[s[i]]
+    return result
+
+
+# calling the result
+result = romanToInt('XX')
+print(result)
+
+
---
+++ test.py
@@ -0,0 +1,35 @@
+import unittest
+
+from main import romanToInt
+
+
+class TestRomanToInt(unittest.TestCase):
+    # test single digit
+    def test_single_digit(self):
+        self.assertEqual(romanToInt('V'), 5)
+        self.assertEqual(romanToInt('X'), 10)
+        self.assertEqual(romanToInt('L'), 50)
+        self.assertEqual(romanToInt('C'), 100)
+        self.assertEqual(romanToInt('D'), 500)
+        self.assertEqual(romanToInt('M'), 1000)
+
+    # test multiple digit
+    def test_multiple_digits(self):
+        self.assertEqual(romanToInt('II'), 2)
+        self.assertEqual(romanToInt('VII'), 7)
+        self.assertEqual(romanToInt('XVI'), 16)
+        self.assertEqual(romanToInt('IV'), 4)
+        self.assertEqual(romanToInt('IX'), 9)
+        self.assertEqual(romanToInt('XIV'), 14)
+
+    # test invalid letter
+    def test_invalid_letter(self):
+        # Test for invalid letter 'Z'
+        result = romanToInt('XXZ')
+        assert result == -1
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
