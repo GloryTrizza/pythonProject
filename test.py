2c0f472114163d5d907c1553ca4d04954434c783
@GloryTrizza <gtrizza.onyango@gmail.com>
2023-03-27 21:07:59 +0300

    change 1 for testing invalid

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ class TestRomanToInt(unittest.TestCase):
         self.assertEqual(romanToInt('D'), 500)
         self.assertEqual(romanToInt('M'), 1000)
 
-    # test multiple digits
+    # test multiple digit
     def test_multiple_digits(self):
         self.assertEqual(romanToInt('II'), 2)
         self.assertEqual(romanToInt('VII'), 7)
@@ -25,9 +25,41 @@ class TestRomanToInt(unittest.TestCase):
     # test invalid letter
     def test_invalid_letter(self):
         # Test for invalid letter 'Z'
-        result = romanToInt('Z')
+        result = romanToInt('XXZ')
         assert result == -1
 
+    # test subtractive notation
+    def test_subtractive_notation(self):
+        self.assertEqual(romanToInt('IV'), 4)
+        self.assertEqual(romanToInt('XIV'), 14)
+
+    # test repetition
+    def test_repetition(self):
+        self.assertEqual(romanToInt('II'), 2)
+
+    # test first number
+    def test_first_number(self):
+        self.assertEqual(romanToInt('I'), 1)
+
+    # test invalid letter
+    def test_invalid_letter(self):
+        with self.assertRaises(ValueError):
+            romanToInt('Z')
+
+    # test invalid and valid letters
+    def test_invalid_and_valid_letter(self):
+        with self.assertRaises(ValueError):
+            romanToInt('XIZ')
+
+
+    # test not valid
+    def test_not_valid(self):
+        with self.assertRaises(ValueError):
+            romanToInt('VV')
+
+    # test null input
+    def test_null(self):
+        self.assertEqual(romanToInt(''), 0)
 
 
 if __name__ == '__main__':
